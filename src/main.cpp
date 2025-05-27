a3ae90beb8c5cfe6ee668be979c18864c92be7bc
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-26 21:22:20 -0500

    agregando display

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+/*
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -13,19 +13,18 @@ char dato = 0;
 
 void config_ADC(void) {
     ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-    ADMUX |= (1 << REFS0); // AVCC como referencia
-}
+    ADMUX |= (1 << REFS0); 
 
 void config_USART(void) {
-    UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); // RX habilitado para recibir comando
-    UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);  // 8 bits
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); 
+    UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01); 
     UBRR0 = 103; // 9600 baudios
 }
 
 ISR(USART_RX_vect) {
     dato = UDR0;
     if (dato == 'a') {
-        ADCSRA |= (1 << ADSC); // inicia conversión
+        ADCSRA |= (1 << ADSC);
     }
 }
 
@@ -37,7 +36,7 @@ ISR(ADC_vect) {
     voltaje1 = ((uint8_t)(value * 10)) % 10;
     voltaje2 = ((uint8_t)(value * 100)) % 10;
 
-    UCSR0B |= (1 << UDRIE0); // habilita transmisión
+    UCSR0B |= (1 << UDRIE0); 
 }
 
 ISR(USART_UDRE_vect) {
@@ -59,7 +58,7 @@ ISR(USART_UDRE_vect) {
             flag = 4;
             break;
         case 4:
-            UDR0 = 13; // retorno de carro
+            UDR0 = 13; // 
             flag = 5;
             break;
         case 5:
@@ -76,7 +75,49 @@ int main(void) {
     sei();
 
     while (1) {
-        // Espera comando 'a' para enviar valor
+        // 
+    }
+}
+*/
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile uint8_t centena = 0, decena = 0, unidad = 0;
+
+void config_USART(void) {
+    UCSR0B = (1 << RXEN0) | (1 << RXCIE0); 
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
+    UBRR0 = 103; 
+}
+
+
+int main(void) {
+    DDRD |= 0xF0;
+    DDRB|= 0x07; 
+    config_USART();
+    sei();
+
+    while (1) {
+        // Mostrar unidad
+        PORTB &= ~0x07;
+        PORTB |= 0x02;            
+        PORTD = (PORTD & 0x0F) | (unidad << 4);
+        _delay_ms(5);
+
+        // Mostrar decena
+        PORTB &= ~0x07;
+        PORTB |= 0x01;           
+        PORTD = (PORTD & 0x0F) | (decena << 4);
+        _delay_ms(5);
+
+        // Mostrar centena
+        PORTB &= ~0x07;
+        PORTB |= 0x04;            
+        PORTD = (PORTD & 0x0F) | (centena << 4);
+        _delay_ms(5);
     }
 }
 
