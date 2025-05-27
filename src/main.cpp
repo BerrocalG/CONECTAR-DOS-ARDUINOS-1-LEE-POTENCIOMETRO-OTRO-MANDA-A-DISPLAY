e59d647e3475e2a2eace1b4d26166224c917ae1c
BerrocalG <guadalupe.berrocal@usil.pe>
2025-05-26 21:18:33 -0500

    separando le codigo en dos partes

--- src/main.cpp
+++ src/main.cpp
@@ -1,120 +1,82 @@
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-float value=0;
-int flag=0;
-char dato =0;
-uint8_t unidad =0;
-uint8_t decena =0;
-uint8_t centena =0;
-uint8_t voltaje0 =0;
-uint8_t voltaje1 =0;
-uint8_t voltaje2 =0;
 
-void config_ADC(void){
-  ADCSRA|=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-  ADMUX|=(1<<REFS0);
+float value = 0;
+int flag = 0;
+uint8_t voltaje0 = 0;
+uint8_t voltaje1 = 0;
+uint8_t voltaje2 = 0;
+char dato = 0;
+
+void config_ADC(void) {
+    ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADMUX |= (1 << REFS0); // AVCC como referencia
 }
 
-void config_USART(void){
-  UCSR0B|=(1<<RXEN0)|(1<<TXEN0)| (1<<RXCIE0);
-  UCSR0C|=(1<<UCSZ00)|(1<<UCSZ01);
-  UBRR0=103;//9600
+void config_USART(void) {
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); // RX habilitado para recibir comando
+    UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);  // 8 bits
+    UBRR0 = 103; // 9600 baudios
 }
 
-ISR(USART_RX_vect){
-  dato=UDR0;
-   if(dato=='a'){
-    ADCSRA|=(1<<ADSC);
-   }
+ISR(USART_RX_vect) {
+    dato = UDR0;
+    if (dato == 'a') {
+        ADCSRA |= (1 << ADSC); // inicia conversión
+    }
 }
 
-ISR(ADC_vect){
-  
-  uint16_t adcvalor = ADC; 
+ISR(ADC_vect) {
+    uint16_t adcvalor = ADC;
     value = (adcvalor * 5.0) / 1023.0;
 
-  uint16_t voltaje = (uint16_t)(value * 100);
-    unidad  = voltaje % 10;
-    decena  = (voltaje / 10) % 10;
-    centena = (voltaje / 100) % 10;
+    voltaje0 = (uint8_t)value; // parte entera
+    voltaje1 = ((uint8_t)(value * 10)) % 10;
+    voltaje2 = ((uint8_t)(value * 100)) % 10;
 
-
-    // Para USART
-    voltaje0 = (uint8_t)value;             // Parte entera
-    voltaje1= ((uint8_t)(value * 10)) % 10; // Primera decimal
-    voltaje2 =((uint8_t)(value * 100)) % 10; // Segunda decimal
-
-  UCSR0B|=(1<<UDRIE0);
+    UCSR0B |= (1 << UDRIE0); // habilita transmisión
 }
 
-ISR(USART_UDRE_vect){
-  switch(flag){
-    case 0:
-    UDR0=voltaje0+'0';
-    flag=1;
-    break;
-    case 1:
-    UDR0=(unsigned char)(46);
-    flag=2;
-    break;
-    case 2:
-    UDR0=voltaje1+'0';
-    flag=3;
-    break;
-    case 3:
-    UDR0=voltaje2+'0';
-    flag=4;
-    break;
-    case 4:
-    UDR0=(unsigned char)(13);
-    flag=5;
-    break;
-    case 5:
-    UDR0=(unsigned char)(10);
-    flag=0;
-    UCSR0B&=~(1<<UDRIE0);
-   _delay_ms(100);
-    break;
-  }
+ISR(USART_UDRE_vect) {
+    switch (flag) {
+        case 0:
+            UDR0 = voltaje0 + '0'; // centena
+            flag = 1;
+            break;
+        case 1:
+            UDR0 = '.'; // separador decimal
+            flag = 2;
+            break;
+        case 2:
+            UDR0 = voltaje1 + '0'; // decena
+            flag = 3;
+            break;
+        case 3:
+            UDR0 = voltaje2 + '0'; // unidad
+            flag = 4;
+            break;
+        case 4:
+            UDR0 = 13; // retorno de carro
+            flag = 5;
+            break;
+        case 5:
+            UDR0 = 10; // nueva línea
+            flag = 0;
+            UCSR0B &= ~(1 << UDRIE0); // desactiva interrupción
+            break;
+    }
 }
 
+int main(void) {
+    config_USART();
+    config_ADC();
+    sei();
 
-int main(void){
-    DDRD |= 0xF0; 
-    DDRB |= 0x07; 
-
-  config_USART();
-  config_ADC();
-  sei();
-  while(1){
-    //de codigos pasados
-    // UNIDAD
-         PORTB &= ~0x07;
-        PORTB |= 0x02;
-        PORTD &= ~0xF0;
-        PORTD = (PORTD & 0x0F) | (unidad << 4);
-        _delay_ms(5);
-
-
-        // DECENA
-        PORTB &= ~0x07;
-        PORTB |= 0x01;
-        PORTD &= ~0xF0;
-        PORTD = (PORTD & 0x0F) | (decena << 4);
-        _delay_ms(5);
-
-
-        // CENTENA
-         PORTB &= ~0x07;
-        PORTB |= 0x04;
-        PORTD &= ~0xF0;
-        PORTD = (PORTD & 0x0F) | (centena << 4);
-        _delay_ms(5);
-
-   
-  }
+    while (1) {
+        // Espera comando 'a' para enviar valor
+    }
 }
-
 
